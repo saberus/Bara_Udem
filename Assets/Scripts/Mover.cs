01a7c71706769706465e68b8991642aef15d0afd
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-01-27 22:36:15 +0100

    created basic player movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -18,14 +18,12 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0))
         {
             MoveToCursore();
         }
-        //Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
 
-
-        //gameObject.GetComponent<NavMeshAgent>().destination = target.position;
+        UpdateAnimator();
     }
 
     private void MoveToCursore()
@@ -37,5 +35,13 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity; //Here we grabing global velocity (Not useful for Animator)
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity); //Takes from global and converts to local
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
