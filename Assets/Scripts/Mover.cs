0c4d01e9d20e78520c1b43df034aa66143318c44
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-01-27 21:25:05 +0100

    simple raycasting

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,6 +7,8 @@ public class Mover : MonoBehaviour
 {
     [SerializeField]
     Transform target;
+
+    Ray lastRay;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,12 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+           lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
         gameObject.GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
