f098574b0aab31f2390bbe445b4ef962b6782366
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-01-30 11:43:24 +0100

    added iAction interface to decouple movement and combat

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -1,10 +1,10 @@
-﻿using RPG.Movement;
-using Unity.VisualScripting;
+﻿using RPG.Core;
+using RPG.Movement;
 using UnityEngine;
 
 namespace RPG.Combat
 {
-    public class Fighter : MonoBehaviour
+    public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField]
         float weaponRange = 2f;
@@ -21,7 +21,7 @@ namespace RPG.Combat
             }
             else
             {
-                GetComponent<Mover>().Stop();
+                GetComponent<Mover>().Cancel();
             }
         }
 
@@ -32,6 +32,7 @@ namespace RPG.Combat
 
         public void Attack(CombatTarget combatTarget)
         {
+            GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.transform;
         }
 
---
+++ Assets/Scripts/Core/ActionScheduler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using RPG.Movement;
+using RPG.Combat;
+
+namespace RPG.Core
+{
+    public class ActionScheduler : MonoBehaviour
+    {
+        IAction currentAction;
+        public void StartAction(IAction action)
+        {
+            if (currentAction == action) return;
+            if (currentAction != null) 
+            {
+                currentAction.Cancel();
+                print(currentAction + " canceled");
+            } 
+
+            currentAction = action;
+        }
+
+    }
+}
