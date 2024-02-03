e391e23ebb1db70a12d8164f8cdc866e62bb4ce2
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-02-03 22:42:12 +0100

    added skybox, added riever, added diff speed for patrol and chaise

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -15,6 +15,8 @@ namespace RPG.Control
         [SerializeField] float waypointDwellTime = 3f;
         [SerializeField] PatrolPath patrolPath;
         [SerializeField] float waypointTolerance = 1f;
+        [Range(0,1)]
+        [SerializeField] float patrolSpeedFraction = 0.2f;
 
         Fighter fighter;
         Health health;
@@ -77,7 +79,7 @@ namespace RPG.Control
             if(timeSinceArrivedAtWaypoint > waypointDwellTime)
             {
                 //this will automatically cancel the fighting
-                mover.StartMoveAction(nextPosition);
+                mover.StartMoveAction(nextPosition, patrolSpeedFraction);
             }
         }
 
