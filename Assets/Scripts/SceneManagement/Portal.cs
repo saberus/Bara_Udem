7496cb47968e966d6a5457ee979cbc2b14433183
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-02-07 23:26:57 +0100

    Implemented cross scene reference

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -7,10 +7,16 @@ using UnityEngine.SceneManagement;
 namespace RPG.SceneManagement
 {
 
+    enum DestinationIdentifier
+    {
+        A, B, C, D
+    }
+
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneIndex = -1;
         [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player")) { return; }
@@ -19,6 +25,12 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+
+            if(sceneIndex < 0)
+            {
+                Debug.LogError("Scene to load is not set");
+                yield break;
+            }
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneIndex);
 
@@ -42,6 +54,7 @@ namespace RPG.SceneManagement
             foreach(Portal portal in FindObjectsOfType<Portal>())
             {
                 if (portal == this) continue;
+                if (portal.destination != destination) continue;
                 return portal;
             }
             return null;
