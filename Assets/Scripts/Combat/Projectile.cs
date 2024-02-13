ef9aa94e796f9df938c9622d73b0015b20332a99
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-02-13 22:08:44 +0100

    implemented dealing damage with arrow, trails, archer enemy, bow and projectile variants

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -9,26 +9,48 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float projectileSpeed = 5f;
+        [SerializeField] bool isHoming = false;
 
         Health target = null;
+        float damage = 0;
+        Vector3 targetLocation;
+        private void Start()
+        {
+            transform.LookAt(GetAimLocation());
+        }
 
         private void Update()
         {
             if(target == null) return;
-            transform.LookAt(GetAimLocation());
+
+            if (isHoming && !target.IsDead())
+            {
+                transform.LookAt(GetAimLocation());
+            }
+            
             transform.Translate(Vector3.forward * projectileSpeed * Time.deltaTime);
         }
 
-        public void SetTarget(Health target)
+        public void SetTarget(Health target, float damage)
         {
             this.target = target;
+            this.damage = damage;
         }
 
+        //Gets middle of the target capsule collider
         private Vector3 GetAimLocation()
         {
             CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
             if(targetCapsule == null) return target.transform.position;
             return target.transform.position + Vector3.up * targetCapsule.height / 2;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Health>() != target) return;
+            if (target.IsDead()) return;
+            target.TakeDamage(damage);
+            Destroy(gameObject);
         }
     }
 }
