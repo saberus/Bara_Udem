44b3788d60ab091d53ea86ce5f103e3a212be8af
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-02-17 00:17:45 +0100

    resources folder and ajustments to pickups

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -6,12 +8,30 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weaponPrefab;
+        [SerializeField] float respawnTime = 5f;
         private void OnTriggerEnter(Collider other)
         {
             if (!other.CompareTag("Player")) { return; }
             print("PICKUP " + name + " TRIGGERED");
             other.GetComponent<Fighter>().EquipWeapon(weaponPrefab);
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            StartCoroutine(HideForSeconds(respawnTime));
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool shouldShow)
+        {
+            GetComponent<SphereCollider>().enabled = shouldShow;
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(shouldShow);
+            }
         }
     }
 
