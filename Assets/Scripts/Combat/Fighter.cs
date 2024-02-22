ad0bb01b4a4bed37b5c56fcccfa057c26e338f2c
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-02-22 13:19:35 +0100

    added leveling up, damage progression

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -3,6 +3,7 @@ using RPG.Movement;
 using RPG.Saving;
 using RPG.Attributes;
 using UnityEngine;
+using RPG.Stats;
 
 namespace RPG.Combat
 {
@@ -100,12 +101,13 @@ namespace RPG.Combat
         void Hit()
         {
             if (target == null) return;
+            float damage = GetComponent<BaseStats>().GetStat(Stat.Damage);
             if (currentWeapon.HasProjectile())
             {
-                currentWeapon.LaunchProjectile(rightHandTransform, leftHandTransform, target, gameObject);
+                currentWeapon.LaunchProjectile(rightHandTransform, leftHandTransform, target, gameObject, damage);
             } else
             {
-                target.TakeDamage(gameObject, currentWeapon.WeaponDamage);
+                target.TakeDamage(gameObject, damage);
             }
         }
 
