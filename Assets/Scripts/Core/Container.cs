e79f195a645b945fa67c3bed0f27ec213fd1edba
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-02-27 23:14:44 +0100

    added calculated path

---
+++ Assets/Scripts/Core/Container.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class Container : MonoBehaviour
+    {
+        [SerializeField] bool isDoubleSided = false;
+        [SerializeField] bool isTrader = false;
+
+
+
+    }
+}
