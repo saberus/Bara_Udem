551cfb46fa8b4acdc97c62b03e9f6c8f3e4bf07d
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-03-02 12:23:04 +0100

    changed audio listener to mic_boom GO on the camera, added sounds to sword and unnarmed

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] UnityEvent onHit;
+
         public void OnHit()
         {
-
+            onHit.Invoke();
         }
+
+
     }
 }
